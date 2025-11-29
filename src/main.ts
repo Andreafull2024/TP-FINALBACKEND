6acf4ef5da110f0b31738d9cb101ddbab457e250
Andreafullstack <andreasanjuan21@gmail.com>
2025-11-29 00:17:15 -0300

    test cors with origin *

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // 🔓 Permitir cualquier origen (solo para pruebas)
   app.enableCors({
-    origin: [
-      'http://localhost:5173', // desarrollo local con Vite
-      'https://tp-finalfrotend.vercel.app', // producción en Vercel
-    ],
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
