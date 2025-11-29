d0a237b523a084c1bd3e33ad4379f588cbbe11e6
Andreafullstack <andreasanjuan21@gmail.com>
2025-11-29 01:13:32 -0300

    force CORS headers and handle OPTIONS

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -6,21 +7,27 @@ async function bootstrap() {
 
   // Middleware para forzar CORS en todas las rutas
   app.use((req, res, next) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     res.setHeader(
       'Access-Control-Allow-Origin',
       'https://tp-finalfrotend.vercel.app',
     );
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     res.setHeader(
       'Access-Control-Allow-Methods',
       'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     );
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     res.setHeader(
       'Access-Control-Allow-Headers',
       'Content-Type, Authorization',
     );
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     res.setHeader('Access-Control-Allow-Credentials', 'true');
 
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     if (req.method === 'OPTIONS') {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       res.status(200).end(); // 👈 responde correctamente al preflight
     } else {
       next();
