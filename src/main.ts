0021a8e06c52314ad1f60376af47d5791d87344c
Andreafullstack <andreasanjuan21@gmail.com>
2025-11-29 15:36:29 -0300

    configurar CORS en backend

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Habilitar CORS globalmente sin restricciones
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://tp-finalfrotend.vercel.app', // tu frontend en Vercel
+      'http://localhost:5173'// tu frontend en desarrollo
+    ],
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+  });
 
-  await app.listen(parseInt(process.env.PORT ?? '3000'));
+  await app.listen(process.env.PORT || 8080);
 }
 void bootstrap();
