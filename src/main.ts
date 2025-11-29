2eaa4b708d7dad9f95ae94ad04975fcc348ccec5
Andreafullstack <andreasanjuan21@gmail.com>
2025-11-29 01:10:48 -0300

    force CORS headers and handle OPTIONS

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,30 @@
-/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Middleware para forzar CORS
+  // Middleware para forzar CORS en todas las rutas
   app.use((req, res, next) => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-    res.header('Access-Control-Allow-Origin', 'https://tp-finalfrotend.vercel.app');
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-    res.header('Access-Control-Allow-Credentials', 'true');
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    res.setHeader(
+      'Access-Control-Allow-Origin',
+      'https://tp-finalfrotend.vercel.app',
+    );
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    );
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization',
+    );
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+
     if (req.method === 'OPTIONS') {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-      return res.sendStatus(200);
+      res.status(200).end(); // 👈 responde correctamente al preflight
+    } else {
+      next();
     }
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    next();
   });
 
   await app.listen(parseInt(process.env.PORT ?? '3000'));
