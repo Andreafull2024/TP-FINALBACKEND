fce8744024c54e41b3f1c8a07a3600edcf200728
Andreafullstack <andreasanjuan21@gmail.com>
2025-11-29 14:04:03 -0300

    fix: habilitar CORS globalmente

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: 'https://tp-finalfrotend.vercel.app', // tu frontend en Vercel
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization',
-    credentials: true,
-  });
+  // Habilitar CORS globalmente sin restricciones
+  app.enableCors();
 
   await app.listen(parseInt(process.env.PORT ?? '3000'));
 }
