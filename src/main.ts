52cfd5bcd00e937ec8ab9fece5bab79119737732
Andreafullstack <andreasanjuan21@gmail.com>
2025-11-29 00:59:22 -0300

    force cors headers manually

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Middleware para forzar CORS
   app.use((req, res, next) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
     res.header('Access-Control-Allow-Origin', 'https://tp-finalfrotend.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
     res.header('Access-Control-Allow-Credentials', 'true');
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    if (req.method === 'OPTIONS') {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+      return res.sendStatus(200);
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     next();
   });
 
