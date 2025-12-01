30bc2ad1195ce02057efb9d54dc176d735233269
Andreafullstack <andreasanjuan21@gmail.com>
2025-12-01 16:18:36 -0300

    build

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-  origin: [
-    'http://localhost:5173',
-    'https://tp-finalfrontend-8g1r4ue.vercel.app',
+    origin: [
+      'http://localhost:5173',
+      'https://tp-finalfrontend-8g1r4ue.vercel.app',
       'https://tp-finalfrontend-l8gbnjwle-andrea-san-juans-projects.vercel.app',
-  ],
-  credentials: true,
-});
+    ],
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT || 3000);
 }
