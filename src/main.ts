64026009a88b69459bec8151d093da124702aa6a
Andreafullstack <andreasanjuan21@gmail.com>
2025-12-05 18:17:49 -0300

    Aplicar CORS con middleware

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors'; // 👈 Importá el paquete
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // ✅ Habilitar CORS para tu frontend en Firebase
-app.enableCors({
-  origin: [
-    'http://localhost:5173',                 // desarrollo local
-    'https://pizzaconmigofinal.web.app',     // producción en Firebase
-  ],
-  methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
-  credentials: true,
-});
-
+  // ✅ Usar el middleware cors en lugar de app.enableCors
+  app.use(
+    cors({
+      origin: [
+        'http://localhost:5173',                 // desarrollo local
+  
+        'https://pizzaconmigofinal.web.app',     // producción en Firebase
+      ],
+      methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
+      credentials: true,
+    }),
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
